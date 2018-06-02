6cd79c1ef786054018691bca4c98defbcfb20e41
mumffy-alternate <mumffy+alternate@gmail.com>
2018-06-01 21:43:12 -0700

    initial changes

---
+++ APM/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>The First Component</div>
+    </div>
+    `
+})
+export class AppComponent {
+  pageTitle: string = 'BorpCo Product Management';
+  myName: string = ''
+}
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title: string = 'Angular: Getting Started';
+// }
